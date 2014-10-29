7292af05a8b8f7468e2f52b40ddb5c68b0b42db5
Jake Cobb <jake.cobb@gatech.edu>
2014-10-29 12:42:21 -0400

    Make sure session information can be serialized and restored.

---
+++ src/edu/gatech/sqltutor/util/NoCacheFilter.java
@@ -0,0 +1,33 @@
+package edu.gatech.sqltutor.util;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Prevent responses from being cached.
+ * See https://stackoverflow.com/a/9994586/1464861
+ */
+public class NoCacheFilter implements Filter {
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+	    HttpServletResponse res = (HttpServletResponse) response;
+	    res.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+	    res.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+	    res.setDateHeader("Expires", 0); // Proxies.
+	    chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig config) throws ServletException { }
+
+	@Override
+	public void destroy() { }
+}
